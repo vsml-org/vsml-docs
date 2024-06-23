2f1363d942a975d6f7ea6dde90e1f0d67120cad2
PigeonsHouse <bullbull1230@gmail.com>
2024-06-23 09:11:14 +0900

    update: コードブロックのファイル名表示に対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import remarkCookieTitles from 'remark-flexible-code-titles';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkCookieTitles],
+  }
+});
