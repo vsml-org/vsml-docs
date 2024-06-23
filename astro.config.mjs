e68ec965fc9f76c383157ff800d0108e4ee28b14
PigeonsHouse <bullbull1230@gmail.com>
2024-06-23 15:51:28 +0900

    add: XSS対策と、映像挿入の手法追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
 import { defineConfig } from 'astro/config';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
+import RehypeVideo from 'rehype-video';
 import remarkCookieTitles from 'remark-flexible-code-titles';
+
+const schema = {
+  ...defaultSchema,
+  attributes: {
+    ...defaultSchema.attributes,
+    div: [
+      ...defaultSchema.attributes.div,
+      ['className', /remark-code-.+/]
+    ],
+    '*': [
+      ...(defaultSchema.attributes['*']),
+      'style',
+    ],
+  },
+}
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkCookieTitles],
+    rehypePlugins: [
+      [rehypeSanitize, schema],
+      [RehypeVideo, {test: /\/videos\/.+\.mp4/}],
+    ],
   }
 });
